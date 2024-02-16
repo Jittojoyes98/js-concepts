1c185d99b7e5994bcb7fecf32f8cde6ac0f74d7e
Jitto Joyes <jittojoyes77@gmail.com>
2024-02-16 10:37:18 +0530

    Add:
    -- Hoisting in js.

---
+++ Hoisting/index.js
@@ -0,0 +1,23 @@
+// namaste js (Basics) 
+console.log(x)
+console.log(getName) // gives undefined as getName is a var in arrow functions.
+// getName()
+
+var x=10;
+
+// if we use arrow function it wont take to the top scope and thus gives the 
+// error
+// var getName=()=>{
+//     console.log("Hi user")
+// }
+
+console.log(getName) // gives fullfunction as getName just the same function.
+// getName()
+function getName(){
+    console.log("Hi user")
+}
+
+// console.log(getName)
+
+// -------------------------------------- //
+
